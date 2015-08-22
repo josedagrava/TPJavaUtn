fe7cfc93e74414b826bd1cb6b67e0d51e82961cc
Jose Dagrava <dagravajose@gmail.com>
2015-08-22 19:31:31 -0300

    Correccion de metodo determinarTurno

--- Tp-Ajedrez/src/negocio/Controladora.java
+++ Tp-Ajedrez/src/negocio/Controladora.java
@@ -1,12 +1,23 @@
 package negocio;
 
 import entidades.Partida;
+import java.util.Random;
 
 public class Controladora {
 
 	public Partida buscarPartida(String dniBlancas, String dniNegras) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public int determinarTurno(String dniBlanca, String dniNegras) {
+		Random r= new Random();
+		if(r.nextDouble()<0.5){
+			return Integer.parseInt(dniBlanca);
+		}
+		else{
+			return Integer.parseInt(dniNegras);
+		}
 	}
 
 }
